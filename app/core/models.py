2b9a05dbb8c04e9dd4a0e4fbd58fa6c1f4b20b40
lowskydev <code@lowsky.dev>
2025-04-09 23:22:43 +0200

    add tag model

--- app/core/models.py
+++ app/core/models.py
@@ -59,7 +59,21 @@ class Recipe(models.Model):
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
 
     def __str__(self):
         """String representation of recipe object"""
         return self.title
+
+
+class Tag(models.Model):
+    """Tag to be used for recipe"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        """String representation of tag object"""
+        return self.name
